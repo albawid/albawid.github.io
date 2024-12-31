6d421590e0cc3e3098b39ce31ae55f975fff09e2
albawid <csodynamax1@gmail.com>
2024-12-31 15:28:28 +0700

    malas merinci perubahan intinya penambahan fitur mode malam, perbaikan typo dan beberapa update dari 2023 ke 2024

--- assets/js/disable-list.js
+++ assets/js/disable-list.js
@@ -1,17 +1,9 @@
 function func() {
     var input = document.getElementById("search-input");
     var listPost = document.getElementById("list-post");
-    // var listProject = document.getElementById("list-project")
-    
     if (input.value==='') {
       listPost.classList.remove("disable") 
     } else {
       listPost.classList.add("disable")
     }
-
-    // if (input.value==='') { // search buat project belum bisa
-    //   listProject.classList.remove("disable")
-    // } else {
-    //   listProject.classList.add("disable")
-    // }
   }
